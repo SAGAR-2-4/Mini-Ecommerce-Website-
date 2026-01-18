a8ea2d13d98eb2c97d6f2717b0706035d4e1261f
Sagar <shanmugalingamnithiyasagar@gmail.com>
2026-01-18 13:32:55 +0530

    Add backend application setup and configured the routes and controller file

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+//Backend application file
+
+//install express 
+const express = require(`express`);
+const app = express();
+
+const dotenv = require(`dotenv`);
+const path = require(`path`)
+dotenv.config({path:path.join(__dirname, 'config', '.env')})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server Listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`);
+
+})
