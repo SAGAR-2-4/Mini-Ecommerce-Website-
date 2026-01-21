6d6f7170c143998e799a809f927db17700853312
Sagar <shanmugalingamnithiyasagar@gmail.com>
2026-01-21 20:23:41 +0530

    feat: add initial database schema feat: add initial API endpoints

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,15 @@ const dotenv = require(`dotenv`);
 const path = require(`path`)
 dotenv.config({path:path.join(__dirname, 'config', '.env')})
 
+const products = require(`./routes/product`);
+const orders = require(`./routes/order`);
+
+app.use(`/api/v1`, products);
+app.use(`/api/v1`, orders);
+
+
+
+
 app.listen(process.env.PORT, () => {
     console.log(`Server Listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`);
 
