1f78c095728ac5c3a95381a2900b8fcf66ec890e
Sagar <shanmugalingamnithiyasagar@gmail.com>
2026-01-23 13:23:33 +0530

    Implementing the search function

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import { useState }  from 'react'
+import {useNavigate} from 'react-router-dom'
+
 
 export default function Search() {
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate(); 
+
+    const handleSearch = ()=>{
+        navigate(`/products?keyword=${searchTerm}`);
+
+    }
+
+
   return (
    
          <div className="input-group">
           <input
             type="text"
             id="search_field"
+            onChange={(e) => setSearchTerm(e.target.value)}
             className="form-control"
             placeholder="Enter Product Name ..."
           />
           <div className="input-group-append">
-            <button id="search_btn" className="btn">
+            <button onClick={handleSearch} id="search_btn" className="btn">
               <i className="fa fa-search" aria-hidden="true"></i>
             </button>
           </div>
