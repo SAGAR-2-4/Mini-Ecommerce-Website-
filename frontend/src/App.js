44c59a48390a7c263263b113209a3b89749e2f48
Sagar <shanmugalingamnithiyasagar@gmail.com>
2026-01-25 12:04:06 +0530

    Implemented Product Details Page and integrated the BE api calls

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/search" element={<Home/>} />
+            <Route path= "/product/:id" element={<ProductDetail/>} />
           </Routes>
         </div>
       </Router>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react'
+import Search from './Search'
+import { Link } from 'react-router-dom'
 
-function App() {
+export default function Header() {
   return (
-    <div className="App">
-      <Router>
-        <div>
-          <Header/>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/search" element={<Home/>} />
-          </Routes>
+    <nav className="navbar row">
+      <div className="col-12 col-md-3">
+        <div className="navbar-brand">
+          <Link to="/"><img width="150px" src="./images/logo.png" /></Link>
         </div>
-      </Router>
-      <Footer/>
-    </div>
-  );
-}
+      </div>
 
-export default App;
+      <div className="col-12 col-md-6 mt-2 mt-md-0">
+       <Search/>
+      </div>
+
+      <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+        <span id="cart" className="ml-3">Cart</span>
+        <span className="ml-1" id="cart_count">2</span>
+      </div>
+    </nav>
+  )
+}
