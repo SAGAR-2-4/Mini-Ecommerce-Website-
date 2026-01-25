84750406f030bf5ab38edf17187fcbd21e31c2e4
Sagar <shanmugalingamnithiyasagar@gmail.com>
2026-01-25 12:34:28 +0530

    Resolve the issue in the Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,17 +2,23 @@ import React from 'react'
 import Search from './Search'
 import { Link } from 'react-router-dom'
 
-function App() {
+export default function Header() {
   return (
     <nav className="navbar row">
       <div className="col-12 col-md-3">
         <div className="navbar-brand">
           <Link to="/"><img width="150px" src="./images/logo.png" /></Link>
         </div>
-      </Router>
-      <Footer/>
-    </div>
-  );
-}
+      </div>
 
-export default App;
+      <div className="col-12 col-md-6 mt-2 mt-md-0">
+       <Search/>
+      </div>
+
+      <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+        <span id="cart" className="ml-3">Cart</span>
+        <span className="ml-1" id="cart_count">2</span>
+      </div>
+    </nav>
+  )
+}
