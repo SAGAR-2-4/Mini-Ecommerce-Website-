6c5a2e473bed837ffabd7e79fa9d1ffe25accc3c
SAGAR-2-4 <110651836+SAGAR-2-4@users.noreply.github.com>
2026-01-25 20:14:31 +0530

    Implementing/cart function (#6)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import Search from './Search'
 import { Link } from 'react-router-dom'
-
-function App() {
+import Cart from '../pages/Cart'
+export default function Header({cartItems}) {
   return (
     <nav className="navbar row">
       <div className="col-12 col-md-3">
         <div className="navbar-brand">
           <Link to="/"><img width="150px" src="./images/logo.png" /></Link>
         </div>
-      </Router>
-      <Footer/>
-    </div>
-  );
-}
+      </div>
 
-export default App;
+      <div className="col-12 col-md-6 mt-2 mt-md-0">
+       <Search/>
+      </div>
+
+      <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+        <Link to="/cart" style={{ textDecoration: 'none' }}>
+        <span id="cart" className="ml-3">Cart</span>
+        <span className="ml-1" id="cart_count">{cartItems.length}</span>
+        </Link>
+      </div>
+    </nav>
+  )
+}
